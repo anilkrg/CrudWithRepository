cf37be521e4fcc450ce10f2977899663eb9a8e6b
Anil Kumar <Anil Kumar@ANIL>
2022-05-12 10:00:30 +0530

    Add project files.

---
+++ CrudWithRepository/Models/Client.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudWithRepository.Models
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Address { get; set; }
+        public int City { get; set; }
+        public string Mobile { get; set; }
+        public string Gender { get; set; }
+    }
+}
